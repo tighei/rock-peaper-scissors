e8f5df2a4fc2125aafb5390c721b28b14a0983bc
tighei <73860134+tighei@oussama.adame12@gmail.com>
2022-12-08 21:14:11 +0100

    a new bottun with score

--- main.js
+++ main.js
@@ -1,28 +1,107 @@
+const rockbtn = document.querySelector(".rock");
+const paperbtn = document.querySelector(".paper");
+const scissorsbtn = document.querySelector(".scissors");
+const out_data = document.querySelector(".outdata");
+let playerScore = 0;
+let computerScore = 0;
+const sp = document.querySelector(".splayer");
+const sc = document.querySelector(".scomput");
+
 function computerPlay() {
   let rps = ["rock", "paper", "scissors"];
   let random = rps[Math.floor(Math.random() * rps.length)];
   return random;
 }
 
-function playRound(playerSelection, computerSelection) {
-  if (playerSelection === computerSelection) {
-    return "It's a draw!";
-  } else if (playerSelection === "rock" && computerSelection === "scissors") {
-    return "You win! Rock beats scissors";
-  } else if (playerSelection === "rock" && computerSelection === "paper") {
-    return "You lose! Paper beats rock";
-  } else if (playerSelection === "paper" && computerSelection === "rock") {
-    return "You win! Paper beats rock";
-  } else if (playerSelection === "paper" && computerSelection === "scissors") {
-    return "You lose! Scissors beat paper";
-  } else if (playerSelection === "scissors" && computerSelection === "paper") {
-    return "You win! Scissors beat paper";
-  } else if (playerSelection === "scissors" && computerSelection === "rock") {
-    return "You lose!Rock beats scissors";
-  }
-}
-
 let computerSelection = computerPlay();
-let playerSelection = prompt("Choose your weapon");
+const playRound = (playerSelection, computerSelection) => {
+  if (playerSelection === computerSelection) {
+    const p = document.createElement("p");
+    p.innerText = `draw you both choose  ${playerSelection}`;
+    out_data.appendChild(p);
+  } else if (playerSelection === "rock" && computerSelection === "scissors") {
+    const p = document.createElement("p");
+    p.innerText = `you win you choose ${playerSelection} `;
+    out_data.appendChild(p);
+    ++playerScore;
+  } else if (playerSelection === "rock" && computerSelection === "paper") {
+    const p = document.createElement("p");
+    p.innerText = `you lose u choose ${playerSelection}`;
+    out_data.appendChild(p);
+    ++computerScore;
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
+    const p = document.createElement("p");
+    p.innerText = ` you  win you choose ${playerSelection}`;
+    out_data.appendChild(p);
+    ++playerScore;
+  } else if (playerSelection === "paper" && computerSelection === "scissors") {
+    const p = document.createElement("p");
+    p.innerText = `you lose u choose ${playerSelection}`;
+    out_data.appendChild(p);
+    ++computerScore;
+  } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    const p = document.createElement("p");
+    p.innerText = `  you win you choose ${playerSelection}`;
+    out_data.appendChild(p);
+    ++playerScore;
+  } else if (playerSelection === "scissors" && computerSelection === "rock") {
+    const p = document.createElement("p");
+    p.innerText = `you lose u choose ${playerSelection}`;
+    out_data.appendChild(p);
+    ++computerScore;
+  }
+  console.log(
+    "1",
+    playerSelection,
+    "2",
+    computerSelection,
+    playerScore,
+    computerScore
+  );
+};
+//
+const score = (playerScore, computerScore) => {
+  sp.innerText = ` player score: ${playerScore}`;
+  sc.innerText = `computer score: ${computerScore}`;
+};
+
+//
+const foris = (playerScore, computerScore) => {
+  if (playerScore === 5) {
+    const h2 = document.createElement("h2");
+    h2.innerText = `you  beat the computer ${playerScore}vs ${computerScore} `;
+    out_data.appendChild(h2);
+  } else if (computerScore === 5) {
+    const h2 = document.createElement("h2");
+    h2.innerText = `you  lose  the computer win ${playerScore}vs ${computerScore} `;
+    out_data.appendChild(h2);
+  }
+};
+//
+rockbtn.addEventListener("click", () => {
+  const computerSelection = computerPlay();
+  const playerSelection = "rock";
+
+  playRound(playerSelection, computerSelection);
+  score(playerScore, computerScore);
+  foris(playerScore, computerScore);
+});
+paperbtn.addEventListener("click", () => {
+  const computerSelection = computerPlay();
+  const playerSelection = "paper";
+  playRound(playerSelection, computerSelection);
+  score(playerScore, computerScore);
+  foris(playerScore, computerScore);
+});
+scissorsbtn.addEventListener("click", () => {
+  const computerSelection = computerPlay();
+  const playerSelection = "scissors";
+  playRound(playerSelection, computerSelection);
+  score(playerScore, computerScore);
+  foris(playerScore, computerScore);
+});
+//
+//
+//
+
 console.log(computerSelection);
-console.log(playRound(playerSelection, computerSelection));
